dfa9752b485d8f24e31660767bf3d98da017ef34
chan000518 <wkdvnddl050701@gmail.com>
2025-04-10 17:40:18 +0900

    Initial commit

---
+++ src/main/java/Bubble/bubblog/BubblogApplication.java
@@ -0,0 +1,13 @@
+package Bubble.bubblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubblogApplication.class, args);
+	}
+
+}

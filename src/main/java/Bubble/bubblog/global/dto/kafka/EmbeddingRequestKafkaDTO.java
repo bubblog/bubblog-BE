d75d2b884914926a2f154c2aeb1a24cbdb7b59df
co-deok <155417430+mdy3722@users.noreply.github.com>
2025-10-02 18:49:44 +0900

    Revert "refactor : 임베딩 처리 요청 기능에 대해 기존 webclient 방식에서 Kafka 프로듀싱 방식으로 리팩토링"

--- src/main/java/Bubble/bubblog/global/dto/kafka/EmbeddingRequestKafkaDTO.java
+++
@@ -1,7 +0,0 @@
-package Bubble.bubblog.global.dto.kafka;
-
-public record EmbeddingRequestKafkaDTO(
-        Long postId,
-        String text,   // 내용
-        EmbeddingType type   // TITLE, CONTENT
-) {}
